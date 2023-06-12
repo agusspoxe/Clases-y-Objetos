fd58672ce518dc9913bf693e9623f622f9342ff1
AGUS\agus <agus>
2023-06-12 11:55:38 +0200

    subo el codido completo y corregido

---
+++ alumno.py
@@ -0,0 +1,23 @@
+class Alumno:
+
+    def __init__(self, nombre, turno, correo):
+        self.nombre = nombre
+        self.correo = correo
+        self.turno = turno
+        self.nota = 0
+
+    def __str__(self):
+        buffer = []
+        buffer.append(f"Alumno: {self.nombre.ljust(8)}\n")
+        buffer.append(f" Turno: {self.turno}\n")
+        buffer.append(f"  Nota: {self.nota}")
+
+        return "".join(buffer)
+
+    def setNota(self, nota):
+        self.nota = nota
+
+    def convocar_examen(self, turno):
+        if self.nota >= 5 and turno == self.turno:
+            print(f"{self.correo}")
+            print(f"    Estimado/a {self.nombre}, su nota media ha sido un {self.nota} ha sivo vd convocado al blablabla")
--- main.py
+++ main.py
@@ -1,46 +1,63 @@
-import random
-class Alumno:
-    def __init__(self, nombre, turno, correo):
-        self.nombre = nombre
-        self.turno = turno
-        self.correo = correo
-        self.nota = 0
+from alumno import Alumno
+from aula import Aula
+from profesor import Profesor
 
-    def set_nota(self, nota):
-        self.nota = nota
+CANT_AULAS = 5
 
-    def describe(self):
-        print((f"Alumno/a {self.nombre} del turno {self.turno} con la nota nota:{self.nota}"))
-        
-    def convocar_examen(self):
-        if self.nota <= 5:   
-            print(f"{self.nombre} tienes que ir al examen del 23 de julio de 2023")
+aulas = []
+for i in range(CANT_AULAS):
+    aulas.append(Aula())
 
-class Aula:
-    def __init__(self):
-        self.alumnos = []
-    
-    def add(self, alumno : Alumno):
-        self.alumnos.append(alumno)
-    
-    def listar(self):
-        for alumno in self.alumnos:
-            alumno.set_nota(random.randrange(0, 10))
-            alumno.describe()
-            
-    def tos_palexamen(self):
-        for alumno in self.alumnos:
-            alumno.convocar_examen()
- 
+profesores = [
+    Profesor("Juanito Balerrama", 0, 10),
+    Profesor("Manolito Pomporretas", 0, 10),
+    Profesor("Teres Rabal", 0, 10),
+]
 
-aula1 = Aula()
-aula1.add(Alumno("juan", "A", "Juan@correo.com"))
-aula1.add(Alumno("brand", "c", "brand@correo.com"))
-aula1.add(Alumno("tiryon", "A", "tiryon@correo.com"))
-aula1.add(Alumno("cersey", "b", "cersey@correo.com"))
-aula1.add(Alumno("arya", "b", "arya@correo.com"))
-aula1.add(Alumno("jhon", "c", "jhon@correo.com"))
+alumnos = [
+    Alumno("Pepito", "A", "pepito@a.com"),
+    Alumno("Lucia", "A", "lucia@a.com"),
+    Alumno("Martita", "A", "martita@b.com"),
+    Alumno("Juanito", "A", "juanito@c.com"),
+    Alumno("Jorgito", "A", "jorgito@c.com"),
+    Alumno("Gloria", "A", "gloria@b.com"),
+    Alumno("Jaimito", "A", "jaimito@c.com"),
+    Alumno("Pepo", "A", "Pepo@c.com"),
+    Alumno("Alberta", "A", "Alberta@c.com"),
+    Alumno("Canada", "A", "Canada@c.com"),
+    Alumno("Adam", "A", "Adam@c.com"),
+    Alumno("Rosita", "A", "Rosita@c.com"),
+    Alumno("Juana", "A", "Juana@c.com"),
+    Alumno("Macarena", "A", "Macarena@c.com"),
+    Alumno("Rigoberto", "A", "Rigobert@c.com"),
+    Alumno("YYYYYYYYto", "A", "YYYYYYYY@c.com"),
+    Alumno("Cristiano", "A", "Cristiano@c.com")
+]
 
-aula1.listar()
-aula1.tos_palexamen()
+aulas[0].set_profesor(profesores[0])
+aulas[1].set_profesor(profesores[1])
+aulas[2].set_profesor(profesores[2])
+aulas[4].set_profesor(profesores[0])
 
+contador = 0
+while contador < len(alumnos):
+    aulas[contador % (len(aulas) - 1)].add(alumnos[contador])
+    contador += 1
+
+contador = 0
+for aula in aulas:
+    print(f"AULA {contador} ----------------------------- ")
+
+    try:
+        aula.puntuar()
+    except Exception as e:
+        print(e)
+
+    try:
+        aula.convocar_examen("A")
+    except Exception as e:
+        aula.listar()
+        print(e)
+
+    print("\n" * 3)
+    contador += 1
